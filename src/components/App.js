19003333cdf5182e204454ccff14c40091ff6f19
Andre Junqueira <andre@MacBook-Pro-de-Andre.local>
2017-09-28 02:20:20 -0300

    Todo app com redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../reducers';
+
 import Home from './Home';
+
+import Navigator from '../config/configureRoutes';
 
 export default class App extends Component {
   render() {
+
+    const store = createStore(reducers);
+
     return (
-        <Home />
+        <Provider store={store}>
+            <Navigator />
+        </Provider>
     );
   }
 }
